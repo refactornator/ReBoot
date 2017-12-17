da5f573605505e7f161d6d9611edb63a06c4867a
William Lindner <wlindner522@gmail.com>
2017-12-16 21:14:12 -0800

    Get webpack dev server to start when backend is started

--- backend/src/main/java/application/Application.java
+++ backend/src/main/java/application/Application.java
@@ -2,8 +2,9 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SessionAutoConfiguration.class)
 public class Application {
 
   public static void main(String[] args) {
