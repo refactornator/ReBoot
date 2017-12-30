cea45e1d7ac5fd825ac5fee67df15373d1bf5176
William Lindner <wlindner522@gmail.com>
2017-12-29 21:00:23 -0800

    Fix source-map warning

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   entry: ['./src/index'],
   devServer: {
     hot: true,
