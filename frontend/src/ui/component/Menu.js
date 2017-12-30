7f96679e309ddd3bc27ff4c302d8b4978c02242e
William Lindner <wlindner522@gmail.com>
2017-12-29 20:07:04 -0800

    Introduce Styled Components 💅🏽

---
+++ frontend/src/ui/component/Menu.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HorizontalMenu = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  white-space: nowrap;
+`;
+
+const MenuList = styled.ul`
+  position: relative;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: inline-block;
+`;
+
+const MenuItem = styled.li`
+  position: relative;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  display: inline-block;
+  *display: inline;
+  zoom: 1;
+  vertical-align: middle;
+`;
+
+const MenuLink = styled(Link)`
+  display: block;
+  text-decoration: none;
+  white-space: nowrap;
+  padding: 0.5em 1em;
+`;
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <HorizontalMenu>
+        <MenuList>
+          <MenuItem>
+            <MenuLink to="/">Home</MenuLink>
+          </MenuItem>
+        </MenuList>
+      </HorizontalMenu>
+    );
+  }
+}
--- frontend/src/ui/container/App.js
+++ frontend/src/ui/container/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const TopMenu = props => {
-  return (
-    <div className="pure-menu pure-menu-horizontal">
-      <ul className="pure-menu-list">
-        <li className="pure-menu-item">
-          <Link to="/" className="pure-menu-link">
-            Home
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
+import Menu from '../component/Menu';
 
 export class App extends Component {
   render() {
     return (
       <div id="application">
-        <TopMenu />
+        <Menu />
         {this.props.children}
       </div>
     );
