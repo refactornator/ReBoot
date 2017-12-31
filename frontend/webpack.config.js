c22a5703f65269f02176c56244c3ce3534d30f65
William Lindner <wlindner522@gmail.com>
2017-12-30 23:28:36 -0800

    Fix webpack devServer proxy ports and output publicPath

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,12 +10,13 @@ module.exports = {
     port: 3000,
     contentBase: path.join(__dirname, '../backend/src/main/resources/static'),
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/graphql': 'http://localhost:3000'
+      '/api': 'http://localhost:8080',
+      '/graphql': 'http://localhost:8080'
     }
   },
   output: {
     path: path.join(__dirname, 'dist'),
+    publicPath: '/',
     filename: '[name].js'
   },
   plugins: [
